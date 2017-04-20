f7b106397a98fcf49e7494170e39be1357e48cd1
Collin Kee <collinkee@gmail.com>
2017-04-20 11:40:15 -0700

    first upload non functional jquery/javascript

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var count = 0,
+    sum = 0,
+    average = 0,
+    string = "";
+
+//need to use jquery for functionality
+$('#submit').click(function (); 
+$('#reset').click(function ();                   
+                  
+//need to use several functions below
+//need to do several if statements                 
